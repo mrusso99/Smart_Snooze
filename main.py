a1e74b0e778a9c8f939187cc0e15538f3525eaaf
SimoneSalzano <s.salzano9@studenti.unisa.it>
2021-05-01 17:13:51 +0200

    Implemented a button that sets off the alarm.

--- main.py
+++ main.py
@@ -2,21 +2,28 @@ from src.libraries.LedControls import Leds
 from src.libraries.BuzzerControls import BuzzControls
 import threading
 
+button = D5
+pinMode(button,INPUT_PULLDOWN)
+
 
 alarm = threading.Event()
 thread(Leds.loop,alarm)
 thread(BuzzControls.sing,2,alarm)
 
-# loop forever
-while True:
-    alarm.set()
-    print("setting...")
-    sleep(10000)
-    alarm.clear()
-    BuzzControls.reset()
-    Leds.reset()
-    print("clearing...")
-    sleep(10000)
 
+def setAlarmOff():
+    """clears the alarm,resets every component involved in it"""
     
+    if alarm.is_set:
+        alarm.clear()
+        Leds.reset()
+        BuzzControls.reset()
+        
+        
+#Whenever the button is pressed, clear the alarm:
+onPinRise(button,setAlarmOff)
 
+# loop forever
+alarm.set()
+#while True:
+    #sleep(5000)
