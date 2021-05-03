9ef1b6d2dbd3f43bc6ec9b61cd9ffc8843b2d143
Marco Russo <28223818+mrusso99@users.noreply.github.com>
2021-05-03 11:15:22 +0200

    Add RTC code

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import maxim.ds1307 import ds1307
+import streams
+streams.serial()
 from src.libraries.LedControls import Leds
 from src.libraries.BuzzerControls import BuzzControls
 import threading
+
+ds=ds1307.DS1307(I2C0)
+ds.start()
 
 button = D5
 pinMode(button,INPUT_PULLDOWN)
@@ -10,6 +16,12 @@ alarm = threading.Event()
 thread(Leds.loop,alarm)
 thread(BuzzControls.sing,2,alarm)
 
+
+def printOnLCD():
+    "LCD todo, function that prints everything we need on the LCD"
+    while True
+        print("%02d:%02d:%02d - %02d/%02d/%d - %d"%ds.get_time())
+        sleep(1000)
 
 def setAlarmOff():
     """clears the alarm,resets every component involved in it"""
