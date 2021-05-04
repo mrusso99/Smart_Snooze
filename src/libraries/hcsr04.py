518c02e69a801ef2db8710b6ab1cbbefdffb7858
mrusso99 <28223818+mrusso99@users.noreply.github.com>
2021-05-04 11:44:38 +0200

    Create hcsr04.py

---
+++ src/libraries/hcsr04.py
@@ -0,0 +1,21 @@
+class hcsr04:
+    
+    @c_native("HCRS04_readDistanceRaw", ["hcsr04.c"], [])
+    def _getDistanceRaw(trig, echo):
+        pass
+    
+    def getDistanceRaw(self):
+        return hcsr04._getDistanceRaw(self.trigger, self.echo)
+    
+    def getDistanceCM(self):
+        return self.getDistanceRaw() / 58
+    
+    def getDistanceINCH(self):
+        return self.getDistanceRaw() / 148
+    
+    def __init__(self, trigger, echo):
+        self.trigger = trigger
+        self.echo = echo
+        
+        pinMode(self.trigger, OUTPUT)
+        pinMode(self.echo, INPUT)
