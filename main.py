a72ea1f896a28eb2a2bc358ffe9b0013c15768dc
Marco Russo <28223818+mrusso99@users.noreply.github.com>
2021-05-18 15:57:35 +0200

    ZDM

--- main.py
+++ main.py
@@ -9,9 +9,16 @@ from src.libraries import lcd
 import threading
 from src.libraries import MemorizedAlarm
 import internet
-
+import fs
+from zdm import zdm
 
 internet.internet.connect()
+
+agent = zdm.Device()
+    # just start it
+agent.connect()
+
+
 
 
 disp = lcd.lcd(i2cport = I2C1)
@@ -19,6 +26,28 @@ disp = lcd.lcd(i2cport = I2C1)
 
 button = D5
 pinMode(button,INPUT_PULLDOWN)
+
+
+
+
+    # read an existing file in read mode
+    f = fs.open("/zerynth/snoozes.txt", "r")
+    # read it all and print it
+    print(f.read()) 
+    # close the file
+    f.close()
+
+    # create a new file and read it back
+    f = fs.open("/zerynth/test02.txt","w")
+    f.write("first row: test 01\n")
+    f.write("second row: test 02\n")
+    f.close()
+
+    f = fs.open("/zerynth/test02.txt", "r")
+    print(f.read())
+    f.close()
+
+    sleep(5000)
 
 
         
@@ -30,7 +59,7 @@ alarm = threading.Event()
 def setAlarmOff():
     """clears the alarm,resets every component involved in it"""
     
-    if alarm.is_set:
+    if alarm.is_set():
         alarm.clear()
         Leds.reset()
         BuzzControls.reset()
@@ -53,5 +82,9 @@ while False:
 while True:
     disp.clear()
     disp.message("%02d:%02d:%02d"%RTC.ds.get_time())
+    temp=DigitalTemperature.read()
     disp.message("%d Celsius"%DigitalTemperature.read(),line = 2)
-    sleep(500)
+    agent.publish({"temperatura":temp}, "temperatura")
+    sleep(5000)
+
+
