27de7a8a30a08f3478c689318aa0eb9661c37c7c
Marco Russo <28223818+mrusso99@users.noreply.github.com>
2021-05-23 18:39:45 +0200

    Mutex on RTC and Temp sensor + more customization

--- main.py
+++ main.py
@@ -13,11 +13,18 @@ import internet
 from zdm import zdm
 
 
+lock_on_RTC= threading.Lock()
+lock_on_temp= threading.Lock()
 
-alarmList = [MemorizedAlarm.MemorizedAlarm(3,16,"magenta",3),MemorizedAlarm.MemorizedAlarm(10,39,"blue",2)]
+alarmList = [MemorizedAlarm.MemorizedAlarm(16,0,"magenta",3),MemorizedAlarm.MemorizedAlarm(10,39,"blue",2)]
 alarm = threading.Event()
 
 
+
+
+
+distanceCM=50
+minsPostponement=5
 
 def on_timestamp(device, timestamp):
     print("RCV time:",timestamp) 
@@ -72,16 +79,44 @@ def setTime(device, arg):
         print("formato corretto")
         RTC.ds.set_time(arg["hours"],arg["minutes"],arg["seconds"],arg["day"],arg["month"],arg["year"],arg["day_of_week"])
     
+def changeDistancePostponement(device, arg):
+    if "distance" in arg:
+        if arg["distance"] > 100 and arg["distance"]< 10:
+            return{"err": "Distance not allowed, pick a value between 10 and 100"}
+            print("Distance not allowed, pick value between 10 and 100")
+        else:
+            distanceCM=arg["distance"]
+            return{"res":"Distance changed, new value:" + distanceCM}
+    else:
+
+        print("Wrong payload format")
         
+def changeMinsPostponement(device, arg):
+    if "minutes" in arg:
+        if arg["minutes"] > 30 and arg["minutes"]<= 0:
+            return{"err": "Error, pick a value under 30 minutes and greater than 0"}
+            print("Error, pick a value under 30 minutes and greater than 0")
+        else:
+            minsPostponement=arg["minutes"]
+            return{"res":"Value changed, new value:" + minsPostponement}
+    else:
+        return{"err":"Wrong payload format"}
+        print("Wrong payload format")
+    
 dict = {
     "readTemp" : DigitalTemperature.read,
     "readTime" : RTC.ds.get_time,
     "insertAlarm" : insertAlarm,
     "deleteAlarm" : deleteAlarm,
-    "setTime": setTime
+    "setTime": setTime,
+    "changeDistancePostponement": changeDistancePostponement,
+    "changeMinsPostponement": changeMinsPostponement
 }
 
 tags = ["temperatura","alarm"]
+
+
+
 
 internet.internet.connect()
 
@@ -89,6 +124,16 @@ device = zdm.Device(jobs_dict = dict,condition_tags = tags, on_timestamp=on_time
     # just start it
 device.connect()
 
+def sendTempToZDM(device,tags):
+    while True:    
+        lock_on_temp.acquire()
+        temp=DigitalTemperature.read()
+        lock_on_temp.release()
+        payload={"temperatura":temp}
+        device.publish(payload, tags[0])
+        sleep(5000)
+
+thread(sendTempToZDM,device,tags)
 
 disp = lcd.lcd(i2cport = I2C1)
 
@@ -110,18 +155,7 @@ def setAlarmOff():
 device.request_timestamp()
 #Whenever the button is pressed, clear the alarm:
 onPinRise(button,setAlarmOff)
-thread(RTC.watchForAlarms,alarmList,alarm)
-
-
-def onChangePublish():
-    while True:
-        old=alarmList
-        sleep(60000)
-        for alarm in alarmList:
-            if alarm not in old:
-                device.publish({"Alarm":alarm}, tags[1])
-
-thread(onChangePublish)
+thread(RTC.watchForAlarms,alarmList,alarm,lock_on_RTC)
 
 
 
@@ -129,11 +163,14 @@ thread(onChangePublish)
 
 while True:
     disp.clear()
+    lock_on_RTC.acquire()
+    print("Lock acquired in main")
     disp.message("%02d:%02d:%02d"%RTC.ds.get_time())
-    temp=DigitalTemperature.read()
+    lock_on_RTC.release()
+    print("Lock released")
+    lock_on_temp.acquire()
     disp.message("%d Celsius"%DigitalTemperature.read(),line = 2)
-    payload={"temperatura":temp}
-    device.publish(payload, tags[0])
-    sleep(5000)
+    lock_on_temp.release()
+    sleep(1000)
 
 
--- src/libraries/RTC.py
+++ src/libraries/RTC.py
@@ -6,34 +6,49 @@ from src.libraries import hcsr04
 from src.libraries import MemorizedAlarm
 import streams
 
+
 ds=ds1307.DS1307(I2C0)
 ds.start()
 prox=hcsr04.hcsr04(23,22)
-def watchForAlarms(alarmsList, alarm):
+
+
+def watchForAlarms(alarmsList, alarm, lock_on_RTC):
     """To be called in a thread, it checks if the current time corresponds to a time in which the alarm should be set on and sets it on"""
     
     while True:
+        lock_on_RTC.acquire()
+        print("Lock acquired on library")
         currentTime = ds.get_time()
+        lock_on_RTC.release()
+        print("lock released")
         for memorizedAlarm in alarmsList:
+            print("Entro nel for")
             if currentTime[0] == memorizedAlarm.hour and currentTime[1] == memorizedAlarm.minute:
-                if not alarm.is_set():
-                    alarm.set()
+                    print("Condizione verificata")
                     #Add the threads to set the LED and the Buzzer properly based on the chosen alarmTime and to check if the user wants a snooze.
                     #thread(hcsr04.checkDelays,memorizedAlarm,alarmsList,alarm)
+ 
                     thread(Leds.setAlarmColor,memorizedAlarm.color,alarm)
+                    print("thread1")
                     thread(BuzzControls.sing,memorizedAlarm.song,alarm)
+                    print("thread2")
                     #Removes the delayedAlarm when the alarm clears.
                     thread(checkDelays,memorizedAlarm,alarmsList,alarm)
+                    print("thread3")
                     if memorizedAlarm.isDelayed:
+                        print("condizione 2")
                         while alarm.is_set():
+                            print("dentro il while")
                             sleep(1000)
                         alarmsList.remove(memorizedAlarm)
+                        print("allarme rimosso")
                     sleep(60000)
         sleep(1000)
         
 
 def checkDelays(memorizedAlarm,alarmsList,alarm):
         #TODO: Check how to get the information about the checkDelay
+        print("entro nel checkdelay")
         while alarm.is_set():
             if delayDetected(10):
                 
@@ -43,14 +58,11 @@ def checkDelays(memorizedAlarm,alarmsList,alarm):
                 alarmsList.append(newAlarm)
                 print(newAlarm.hour, newAlarm.minute)
 
+
+
+
 def delayDetected(distanceCM):
     if prox.getDistanceCM() < distanceCM:
         return True
 
         
-def printOnLCD():
-    "TODO: LCD function that prints everything we need on the LCD"
-    
-    while True:
-        print("%02d:%02d:%02d - %02d/%02d/%d - %d"%ds.get_time())
-        sleep(1000)
